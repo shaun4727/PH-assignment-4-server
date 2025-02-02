3f279f5c0718435d68fc667500b0f4a8ee959228
Shaun Hossain <107396795+shaun4727@users.noreply.github.com>
2025-02-03 02:38:07 +0600

    cors added

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,18 @@ import router from "./app/routes";
 const app: Application = express();
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ph-assignment-4-client-book-shop.vercel.app/",
+    ],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", router);
