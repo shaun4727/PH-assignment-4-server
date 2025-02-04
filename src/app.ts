a900f9c962dc373a206b75737e1c8a5598d809ab
Shaun Hossain <107396795+shaun4727@users.noreply.github.com>
2025-02-04 19:16:10 +0600

    up

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
-    // origin: ["https://ph-assignment-4-client-book-shop.vercel.app"],
+    // origin: ["http://localhost:5173"],
+    origin: ["https://ph-assignment-4-client-book-shop.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
