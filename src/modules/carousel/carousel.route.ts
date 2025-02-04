8b61d60bf30665d7fb49b3001297355743b60f9d
Shaun Hossain <107396795+shaun4727@users.noreply.github.com>
2025-02-04 22:53:47 +0600

    up

--- src/modules/carousel/carousel.route.ts
+++ src/modules/carousel/carousel.route.ts
@@ -1,16 +1,29 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import auth from "../../app/middleware/auth";
 import { upload } from "../../app/utils/sendImageToCloudinary";
 import { USER_ROLE } from "../user-auth/user_auth.constant";
 import { CarouselController } from "./carousel.controller";
-import { makeFolder } from "../../app/utils/makeDir";
+const fs = require("fs");
+const path = require("path");
 
 const router = express.Router();
 
 router.post(
   "/create-carousel",
   auth(USER_ROLE.admin),
-  makeFolder,
+  (req: Request, res: Response, next: NextFunction) => {
+    const folderName = "uploads";
+    // Create the full path to the folder
+    const folderPath = path.join(process.cwd(), folderName);
+
+    // Create the folder if it doesn't already exist
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
+
+    // Proceed to the next middleware
+    next();
+  },
 
   upload.array("file"),
 
